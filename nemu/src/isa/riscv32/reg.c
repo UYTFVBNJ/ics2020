2a26cb89cc81c0debaa8359d5e43e7cb1312d16f
tracer-ics2020 <tracer@njuics.org>
2020-09-17 10:29:45 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: b13de41485b14be2b891e2cfe0ab8c2d   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  10:29:45 up 19:08,  1 user,  load average: 1.17, 1.20, 1.32

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -11,11 +11,11 @@ const char *regsl[] = {
 void isa_reg_display() {
 	puts("REGs: ");
 	for (int i=0;i<4;i++) {
-		for (int j=0;j<8;j++)	printf("|%8s|",regsl[i]); 
+		for (int j=0;j<8;j++)	printf("|%8s|",regsl[4*i+j]); 
 		puts("");
-		for (int j=0;j<8;j++)	printf("%08X",reg_l(i)); 
+		for (int j=0;j<8;j++)	printf("%08X",reg_l(4*i+j)); 
 		puts("");
-		for (int j=0;j<8;j++)	printf("|%8d|",reg_l(i)); 
+		for (int j=0;j<8;j++)	printf("|%8d|",reg_l(4*i+j)); 
 		puts("");
 
 	}
