3fe4eca0391439d942c00f3bdf81505af80008fb
tracer-ics2020 <tracer@njuics.org>
2020-09-23 21:44:44 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 77dc79d768de4323a95869561c817676   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  21:44:44 up  2:29,  1 user,  load average: 0.58, 0.57, 0.58

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -2,7 +2,43 @@ void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
 
+// GH: own changes
+//#include "expr.h"
+#include <stdio.h>
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+uint32_t expr();
+char buf[66000];
+// GH: own changes
+
 int main(int argc, char *argv[]) {
+
+// GH: own changes
+	
+	FILE *fp = fopen("../tools/gen-expr/input","r");
+	assert(fp != NULL);
+
+	unsigned int result,i,exp; bool success=0;
+
+	for (i=0;i<1;i++) {
+
+		if (~fscanf(fp, "%u", &result)) {
+			puts("ERR FSC");
+			return 0;	
+		}
+		if (fgets(buf, 65900, fp) == NULL) {
+			puts("ERR FGT");
+			return 0;
+		}
+
+		exp=expr(buf, success);
+
+		printf("%u %u %d\n",result, exp, success);
+	}
+// GH: own changes
+
 
   /* Initialize the monitor. */
   init_monitor(argc, argv);
