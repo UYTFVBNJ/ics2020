35d062def998f0210d04c736b51d06e6239dcb3b
tracer-ics2020 <tracer@njuics.org>
2020-09-25 17:10:51 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 7e57346896184c12bd28abb7fcdcaedd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  17:10:51 up  6:25,  1 user,  load average: 1.21, 1.13, 1.08

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -14,7 +14,7 @@ char buf[66000];
 // GH: own changes
 
 int main(int argc, char *argv[]) {
-/*
+	
 // GH: own changes
 	
 	FILE *fp = fopen("/home/gh/ics2020/nemu/tools/gen-expr/input","r");
@@ -45,7 +45,6 @@ int main(int argc, char *argv[]) {
 		printf("%u %u %d\n",result, exp, success);
 	}
 // GH: own changes
-*/
 
   /* Initialize the monitor. */
   init_monitor(argc, argv);
