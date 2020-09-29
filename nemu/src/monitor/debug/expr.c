695ab6bd4f1aad9d2d50540e09d61de13e0e57b0
tracer-ics2020 <tracer@njuics.org>
2020-09-29 22:05:18 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 09ad53c11ebb4def8a570cc224e9d2b2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  22:05:19 up  2:27,  1 user,  load average: 0.59, 0.68, 0.59

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -139,7 +139,8 @@ word_t eval(int p, int q, bool *success) {
 		*success = 0;
 		return 0;
 	} else if (p==q) {
-		return strtol(tokens[p].str,NULL,10); 
+    unsigned ans= strtol(tokens[p].str,NULL,10); 
+		return ans;
 	} else if (check_parentheses(p,q,success)) {
 		return eval(p+1,q-1,success);
 	} else if (*success) {
