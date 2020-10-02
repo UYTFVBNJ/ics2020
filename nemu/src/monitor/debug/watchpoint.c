68a2e92adb850e490daabdc30671bba43c5cadec
tracer-ics2020 <tracer@njuics.org>
2020-10-02 11:16:16 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 0933109c9eff491e8eb41eadb9512528   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  11:16:16 up  1:27,  1 user,  load average: 0.46, 0.40, 0.44

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -13,6 +13,7 @@ void init_wp_pool() {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
     // wp_pool[i].exp
+    wp_pool[i].last_result = 0;
   }
   wp_pool[NR_WP - 1].next = NULL;
 
@@ -68,6 +69,30 @@ void free_wp(int wp_no) {
 void wp_display() {
   for (int i = 0; i < 32; i ++) if (wp_pool[i].enable) {
     printf("Num  Type  Disp  Enb  Address  What\n");
-    printf("%3d expr  keep  y    unknown  %s\n", i, wp_pool[i].exp);
+    printf("%3d  expr  keep  y    unknown  %s\n", i, wp_pool[i].exp);
   }
+}
+
+bool wp_check() {
+  bool ret = 0;
+  
+  for (WP *p = head; p != NULL; p = p->next) {
+    word_t result; bool success = 1;
+    result = expr(p->exp, &success);
+    if (!success) Log("ERR: invalid expression");
+
+    if (result != p->last_result) {
+      puts("Old value = ");
+      printf("%u\n",p->last_result);
+
+      puts("New value = ");
+      printf("%u\n",result);
+
+      p->last_result = result;
+
+      ret = 1;
+    }
+
+  }
+  return ret;
 }
