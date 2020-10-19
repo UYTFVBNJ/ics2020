8e9ddc47908dbc4a854afb525769da272e331415
tracer-ics2020 <tracer@njuics.org>
2020-10-19 20:49:16 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 4152d1f61c9a46f6ab649eee53217680   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  20:49:16 up 36 min,  1 user,  load average: 0.37, 0.46, 0.49

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -3,11 +3,44 @@
 
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
+/*
 static const uint32_t img [] = {
   0x800002b7,  // lui t0,0x80000
   0x0002a023,  // sw  zero,0(t0)
   0x0002a503,  // lw  a0,0(t0)
   0x0000006b,  // nemu_trap
+};
+*/
+
+static const uint32_t img [] = {
+  0x00000413,
+  0x00009117,                                                                                                                                                                                               
+  0xffc10113,                                                                                                                                                                                               
+  0x024000ef,                                                                                                                                                                                               
+                                                                                                                                                                                                         
+  // main>:  ,                                                                                                                                                                                               
+  0x00000513,                                                                                                                                                                                               
+  0x00008067,                                                                                                                                                                                               
+                                                                                                                                                                                                         
+  // putch>: ,                                                                                                                                                                                               
+  0xa10007b7,                                                                                                                                                                                               
+  0x3ea78c23,                                                                                                                                                                                               
+  0x00008067,                                                                                                                                                                                               
+                                                                                                                                                                                                         
+  // halt>:  ,                                                                                                                                                                                               
+  0x00050513,                                                                                                                                                                                               
+  0x0000006b,                                                                                                                                                                                               
+  0x0000006f,                                                                                                                                                                                               
+                                                                                                                                                                                                         
+  // _trm_ini,                                                                                                                                                                                               
+  0x80100537,                                                                                                                                                                                               
+  0xff010113,                                                                                                                                                                                               
+  0x05050513,                                                                                                                                                                                               
+  0x00112623,                                                                                                                                                                                               
+  0xfd1ff0ef,                                                                                                                                                                                               
+  0x00050513,                                                                                                                                                                                               
+  0x0000006b,                                                                                                                                                                                               
+  0x0000006f,
 };
 
 static void restart() {
