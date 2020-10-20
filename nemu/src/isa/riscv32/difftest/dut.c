48dde64596fec60a6ad61d08ec92658c1f77b7e5
tracer-ics2020 <tracer@njuics.org>
2020-10-20 19:25:05 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 4152d1f61c9a46f6ab649eee53217680   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  19:25:06 up 23:12,  1 user,  load average: 1.25, 1.25, 1.24

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -4,7 +4,11 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  for (int i = 0; i < 32; i ++) if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) return 0;
+  for (int i = 0; i < 32; i ++) if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
+    printf("%d %u %u\n", i, ref_r->gpr[i]._32, cpu.gpr[i]._32);
+    printf("%d %x %x\n", i, ref_r->gpr[i]._32, cpu.gpr[i]._32);
+    return 0;
+  }
   return 1;
 }
 
