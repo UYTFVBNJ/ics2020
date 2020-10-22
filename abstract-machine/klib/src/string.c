1b21365812b83f58ef25cebb5e44c35b741a6d98
tracer-ics2020 <tracer@njuics.org>
2020-10-22 22:40:27 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 40d35412aa1045b6aae9cfc486ccbe5f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  22:40:27 up  6:05,  1 user,  load average: 0.21, 0.51, 0.57

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,43 +4,98 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t i;
+
+  for (i = 0; s[i] != 0; i++) ;
+
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  size_t i;
+
+  for (i = 0; src[i] != '\0'; i++) dst[i] = src[i];
+  dst[i] = '\0';
+
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  size_t i;
+ 
+  for (i = 0; i < n && src[i] != '\0'; i++) dst[i] = src[i]; 
+  for ( ; i < n; i++) dst[i] = '\0';
+
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t dst_len = strlen(dst);
+  size_t i;
+
+  for (i = 0; src[i] != '\0'; i++) dst[dst_len + i] = src[i];
+  dst[dst_len + i] = src[i];
+
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  size_t i;
+
+  for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++) ;
+
+  if (s1[i] > s2[i]) return 1;
+  else if (s1[i] == s2[i]) return 0;
+  else return -1;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  size_t i;
+
+  for (i = 0; i < n && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++) ;
+
+  if (s1[i] > s2[i]) return 1;
+  else if (s1[i] == s2[i]) return 0;
+  else return -1;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  size_t i;
+
+  for (i = 0; i < n; i++) ((int*)v)[i] = c;
+
+  return v;
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
-  return NULL;
+  size_t i;
+  char *p = malloc(n);
+
+  if (p == NULL) return NULL;
+
+  for (i = 0; i < n; i++) p[i] = ((char*)src)[i];
+  for (i = 0; i < n; i++) ((char*)dst)[i] = p[i];
+
+
+  return dst;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  size_t i;
+  
+  for (i = 0; i < n; i++) ((char*)out)[i] = ((char*)in)[i];
+
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  return 0;
+  size_t i;
+  
+  for (i = 0; i < n && ((unsigned char*)s1)[i] == ((unsigned char*)s2)[i]; i++) ;
+  
+  if (((unsigned char*)s1)[i] > ((unsigned char*)s2)[i]) return 1;
+  else if (((unsigned char*)s1)[i] == ((unsigned char*)s2)[i]) return 0;
+  else return -1;
 }
 
 #endif
