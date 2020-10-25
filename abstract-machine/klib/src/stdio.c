6bcba742f571ae6909568ed8f5117691d43e1bb2
tracer-ics2020 <tracer@njuics.org>
2020-10-25 16:10:10 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: e54151ca2072490db3f1664f0378e9d9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  16:10:10 up 1 day,  5:40,  1 user,  load average: 0.92, 0.65, 0.52

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,11 +5,76 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+
+#define __vsprintf()                                                   \
+  size_t fmtp = 0, outp = 0, bufp = 0;                                 \
+  char buf[15];                                                        \
+                                                                       \
+  int v_int;                                                           \
+  char *v_str;                                                         \
+                                                                       \
+                                                                       \
+  for (fmtp = 0; fmt[fmtp] != '\0'; fmtp++) switch (fmt[fmtp]) {       \
+    case '%':                                                          \
+    fmtp++;                                                            \
+    switch (fmt[fmtp]) {                                               \
+      case 'd':                                                        \
+        v_int = va_arg(ap, int);                                       \
+        bufp = 0;                                                      \
+                                                                       \
+        if (v_int < 0) {                                               \
+          v_int = -v_int;                                              \
+          opt('-');                                                    \
+        }                                                              \
+                                                                       \
+        while (v_int) {                                                \
+          buf[bufp++] = v_int % 10 + '0';                              \
+          v_int /= 10;                                                 \
+        }                                                              \
+                                                                       \
+        while (bufp) {                                                 \
+          opt(buf[--bufp]);                                            \
+        }                                                              \
+                                                                       \
+        break;                                                         \
+      case 's':                                                        \
+        v_str = va_arg(ap, char *);                                    \
+        size_t i;                                                      \
+                                                                       \
+        for (i = 0; v_str[i] != '\0'; i++) opt(v_str[i]);              \
+                                                                       \
+        break;                                                         \
+      default:                                                         \
+        printf("Not implemented!");                                    \
+        return -1;                                                     \
+        break;                                                         \
+    }                                                                  \
+    break;                                                             \
+                                                                       \
+    case '\\':                                                         \
+      fmtp++;                                                          \
+      switch (fmt[fmtp]) {                                             \
+        case 'n':                                                      \
+          opt('\n');                                                   \
+          break;                                                       \
+        default:                                                       \
+          printf("Not implemented!");                                  \
+          return -1;                                                   \
+          break;                                                       \
+      }                                                                \
+      break;                                                           \
+    default:                                                           \
+      opt(fmt[fmtp]);                                                  \
+    break;                                                             \
+  }                                                                    \
+                                                                       \
+
 int printf(const char *fmt, ...) {
   va_list vl;
   va_start(vl,fmt);
   char buf[256];
   
+#define opt(arg) out[outp++] = arg
   int outp = vsprintf(buf, fmt, vl);
 
   va_end(vl);
@@ -18,69 +83,13 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  size_t fmtp = 0, outp = 0, bufp = 0;
-  char buf[15];
-
-  int v_int; 
-  char *v_str;
-
-
-  for (fmtp = 0; fmt[fmtp] != '\0'; fmtp++) switch (fmt[fmtp]) {
-    case '%':
-    fmtp++;
-    switch (fmt[fmtp]) {
-      case 'd':
-        v_int = va_arg(ap, int);
-        bufp = 0;
-
-        if (v_int < 0) {
-          v_int = -v_int;
-          out[outp++] = '-';
-        }
-
-        while (v_int) {
-          buf[bufp++] = v_int % 10 + '0';
-          v_int /= 10;
-        }
-
-        while (bufp) {
-          out[outp++] = buf[--bufp];
-        }
-
-        break;
-      case 's':
-        v_str = va_arg(ap, char *);
-        size_t i;
-
-        for (i = 0; v_str[i] != '\0'; i++) out[outp + i] = v_str[i];
-        outp += i;
-
-        break;
-      default:
-        printf("Not implemented!");
-        return -1;
-        break;
-    }
-    break;
   
-    case '\\':
-      fmtp++;
-      switch (fmt[fmtp]) { 
-        case 'n':
-          out[outp++] = '\n';
-          break;
-        default:
-          printf("Not implemented!");
-          return -1;
-          break;
-      }
-      break;
-    default:
-      out[outp++] = fmt[fmtp];
-    break;
-  }
+#define opt(arg) out[outp++] = arg
 
-  out[outp] = '\0';
+  __vsprintf()
+  out[outp] = '\0';                                                    
+
+#undef opt
 
   return outp;
 }
@@ -102,7 +111,7 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  
+
   return 0;
 }
 
