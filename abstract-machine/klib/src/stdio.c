d53414bb7ad5c7f7558d95506616dd97a463675c
tracer-ics2020 <tracer@njuics.org>
2020-10-25 20:08:03 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: e54151ca2072490db3f1664f0378e9d9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  20:08:03 up 1 day,  9:38,  1 user,  load average: 0.69, 0.65, 0.51

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,6 +10,7 @@
   size_t fmtp = 0, outp = 0, bufp = 0;                                 \
   char buf[15];                                                        \
                                                                        \
+  char v_char;                                                         \
   int v_int;                                                           \
   char *v_str;                                                         \
                                                                        \
@@ -18,6 +19,10 @@
     case '%':                                                          \
     fmtp++;                                                            \
     switch (fmt[fmtp]) {                                               \
+      case 'c':                                                        \
+        v_char = va_arg(ap, int);                                     \
+        opt(v_char);                                                   \
+      break;                                                           \
       case 'd':                                                        \
         v_int = va_arg(ap, int);                                       \
         bufp = 0;                                                      \
