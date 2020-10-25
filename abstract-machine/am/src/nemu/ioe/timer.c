ba93271d07de73a144d76450f90fa64102d997f5
tracer-ics2020 <tracer@njuics.org>
2020-10-25 20:00:45 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: e54151ca2072490db3f1664f0378e9d9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  20:00:45 up 1 day,  9:30,  1 user,  load average: 0.15, 0.39, 0.41

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -5,7 +5,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  long seconds = inl(RTC_ADDR + 4);
+  long useconds = inl(RTC_ADDR);
+  uptime->us = seconds * 1000000 + (useconds + 500);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
