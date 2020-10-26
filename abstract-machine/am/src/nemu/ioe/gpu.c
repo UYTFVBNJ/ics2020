514576ab73ff6bef755b10aa177e7293f8745943
tracer-ics2020 <tracer@njuics.org>
2020-10-26 22:41:48 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: b960f7368743483d857b721b142d64d1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  22:41:49 up  1:09,  1 user,  load average: 0.42, 0.37, 0.37

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -3,16 +3,8 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static AM_GPU_CONFIG_T AGC;
 
-void __am_gpu_init() {
-  int i;
-  int wh_info = inl(VGACTL_ADDR);
-  int w = wh_info >> 16;  
-  int h = wh_info & 0xffff;  
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 0);
-}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   int wh_info = inl(VGACTL_ADDR);
@@ -27,7 +19,11 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+  for (int i = 0; i < ctl->h; i++)
+    for (int j = 0; j < ctl->w; j++) fb[(ctl->x + i) * AGC.width + (ctl->y + j)] = ((uint32_t *)ctl->pixels)[i * ctl->w + j];
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
@@ -35,4 +31,18 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
+}
+
+void __am_gpu_init() {
+  __am_gpu_config(&AGC);
+
+  /*
+  int i;
+  int wh_info = inl(VGACTL_ADDR);
+  int w = wh_info >> 16;  
+  int h = wh_info & 0xffff;  
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 0);
+  */
 }
