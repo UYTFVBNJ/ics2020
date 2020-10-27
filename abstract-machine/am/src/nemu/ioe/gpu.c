4ae832c9a46179933dea60cd185af22ec443f06b
tracer-ics2020 <tracer@njuics.org>
2020-10-27 10:27:13 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: b960f7368743483d857b721b142d64d1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  10:27:13 up 12:54,  1 user,  load average: 0.26, 0.65, 0.79

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -37,14 +37,4 @@ void __am_gpu_status(AM_GPU_STATUS_T *status) {
 
 void __am_gpu_init() {
   __am_gpu_config(&AGC);
-
-  /*
-  int i;
-  int wh_info = inl(VGACTL_ADDR);
-  int w = wh_info >> 16;  
-  int h = wh_info & 0xffff;  
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 0);
-  */
 }
