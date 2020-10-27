029df522f5b025a1926e193444881b09b9851145
tracer-ics2020 <tracer@njuics.org>
2020-10-27 09:38:06 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: b960f7368743483d857b721b142d64d1   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  09:38:06 up 12:05,  1 user,  load average: 1.28, 1.57, 1.50

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -21,10 +21,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 
-  for (int i = 0; i < ctl->w; i++)
-    for (int j = 0; j < ctl->h; j++) 
-      if (ctl->x + i < AGC.width && ctl->y + j < AGC.height) 
-        fb[(ctl->x + i) * AGC.height/8 + (ctl->y + j)] = ((uint32_t *)ctl->pixels)[i * ctl->h + j];
+  // for (int i = 0; i < ctl->w; i++)
+    // for (int j = 0; j < ctl->h; j++) 
+      // if (ctl->x + i < AGC.width && ctl->y + j < AGC.height) 
+        // fb[(ctl->x + i) * AGC.height/8 + (ctl->y + j)] = ((uint32_t *)ctl->pixels)[i * ctl->h + j];
+
+  for (int i = 0; i < AGC.width; i++)
+    for (int j = 0; j < AGC.height; j++) 
+      // if (ctl->x + i < AGC.width && ctl->y + j < AGC.height) 
+        fb[i * AGC.height + (ctl->y + j)] = 1;
 
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
