b5486ab46ec67e9c872de9bbc822c0658f7a1768
tracer-ics2020 <tracer@njuics.org>
2020-12-05 13:27:11 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: ed78a21c0dcd48f2a47bc22baf9217b0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  13:27:11 up 14:29,  1 user,  load average: 0.68, 0.71, 0.65

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -7,6 +7,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t epc) {
    */
 
   rtl_li(s, &cpu.csr[2]._32, epc); // sepc
+  printf("NO: %d\n", NO);
   rtl_li(s, &cpu.csr[3]._32, NO);  // scause
   rtl_jr(s, &cpu.csr[1]._32);      // stvec
 
