04b78d5946d50f27dbb7f84b6191143c52a13f3f
tracer-ics2020 <tracer@njuics.org>
2020-12-05 13:40:00 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: ed78a21c0dcd48f2a47bc22baf9217b0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  13:40:00 up 14:42,  1 user,  load average: 0.56, 0.58, 0.62

--- abstract-machine/am/src/nemu/isa/riscv32/cte.c
+++ abstract-machine/am/src/nemu/isa/riscv32/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   printf("HIT!\n");
   printf("%d %d %d\n", c->epc, c->status, c->cause);
-  // for (int i = 0; i < 32; i ++) printf("%d ", c->gpr[i]); 
+  for (int i = 0; i < 32; i ++) printf("%d ", c->gpr[i]); 
   // printf("\n");
   // for (int i = 0; i < 32; i ++) printf("%d ", &c->gpr[i]); 
   // printf("\n");
