08eadbb3374a891493f26332d17a622174bfc7c8
tracer-ics2020 <tracer@njuics.org>
2020-12-05 13:47:50 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: ed78a21c0dcd48f2a47bc22baf9217b0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  13:47:50 up 14:50,  1 user,  load average: 0.72, 0.74, 0.68

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: panic("event yield ID = %d", e.event);
+    case EVENT_YIELD: printf("event yield ID = %d", e.event);
 
     default: panic("Unhandled event ID = %d", e.event);
   }
