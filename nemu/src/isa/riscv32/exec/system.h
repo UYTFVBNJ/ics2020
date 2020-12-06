7b135387b2e064cbb703b0f07f777230f3aad91e
tracer-ics2020 <tracer@njuics.org>
2020-12-06 17:42:13 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 86a175c91d7242ce9cdbaed20931bc68   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  17:42:13 up 22:49,  1 user,  load average: 0.47, 0.50, 0.53

--- nemu/src/isa/riscv32/exec/system.h
+++ nemu/src/isa/riscv32/exec/system.h
@@ -46,6 +46,7 @@ static inline def_EHelper(CSRRCI) {
 
 static inline def_EHelper(SRET) {
   rtl_jr(s, &cpu.csr[2]._32);
+  for (int i = 10; i < 18; i ++) printf("%d ", cpu.gpr[i]._32);
 
   print_asm_template3(sret);
 }
