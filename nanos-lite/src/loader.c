467f826699680c85a791e1aa2992198b477df96d
tracer-ics2020 <tracer@njuics.org>
2020-12-09 21:15:21 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 56c704a68dab4f48a2e753462a1ed525   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  21:15:22 up 26 min,  1 user,  load average: 0.69, 0.56, 0.51

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,33 +9,39 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  // printf("loading:\n");
+  int fd = fs_open(filename, 0, 0);
+  printf("loading:\n");
   static Elf_Ehdr eh;
   static Elf_Phdr ph;
 
-  ramdisk_read(&eh, 0, 52);
+  fs_read(fd, &eh, 52);
 
   //
-  // printf("eh: %d\n", eh.e_phoff);
-  // printf("eh: %d\n", eh.e_phentsize);
-  // printf("eh: %d\n", eh.e_phnum);
+  printf("eh: %d\n", eh.e_phoff);
+  printf("eh: %d\n", eh.e_phentsize);
+  printf("eh: %d\n", eh.e_phnum);
 
   for (int i = 0; i < eh.e_phnum; i ++) {
-    ramdisk_read(&ph, eh.e_phoff + i * eh.e_phentsize, eh.e_phentsize);
+    fs_lseek(fd, eh.e_phoff + i * eh.e_phentsize, 0);
+    fs_read(fd, &ph, eh.e_phentsize);
 
     //
-    // printf("ph: %d\n", ph.p_type);
-    // printf("ph: %p\n", ph.p_offset);
-    // printf("ph: %p\n", ph.p_vaddr);
-    // printf("ph: %d\n", ph.p_filesz);
-    // printf("ph: %d\n", ph.p_memsz);
-    // printf("ph: \n");
+    printf("ph: %d\n", ph.p_type);
+    printf("ph: %p\n", ph.p_offset);
+    printf("ph: %p\n", ph.p_vaddr);
+    printf("ph: %d\n", ph.p_filesz);
+    printf("ph: %d\n", ph.p_memsz);
+    printf("ph: \n");
     
     if (ph.p_type == 1) {
-      ramdisk_read((void(*))ph.p_vaddr, ph.p_offset, ph.p_filesz);
+      fs_lseek(fd, ph.p_offset, 0);
+      fs_read(fd, (void(*))ph.p_vaddr, ph.p_filesz);
 
       for (size_t j = ph.p_vaddr + ph.p_filesz; j < ph.p_vaddr + ph.p_memsz; j ++) {
         *((uint32_t *)j) = 0;
