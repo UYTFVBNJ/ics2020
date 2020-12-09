a86c03a8e1b8b2ccd0ae59ef8221d476aea50047
tracer-ics2020 <tracer@njuics.org>
2020-12-09 20:54:01 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 56c704a68dab4f48a2e753462a1ed525   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  20:54:01 up 5 min,  1 user,  load average: 0.73, 0.76, 0.41

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -56,9 +56,7 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  printf("CALLING\n");
-  _syscall_(SYS_open, (intptr_t)path, flags, mode);
-  return 0;
+  return _syscall_(SYS_open, (intptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count) {
