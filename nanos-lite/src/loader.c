45e3c4703795f4ad58ba0c88c0d29d8d3d9c3d28
tracer-ics2020 <tracer@njuics.org>
2020-12-09 17:54:05 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 8f399a9951f8443a866f5e1780981f90   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  17:54:05 up 22:05,  1 user,  load average: 1.67, 0.75, 0.63

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,27 +12,27 @@
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  printf("loading:\n");
+  // printf("loading:\n");
   static Elf_Ehdr eh;
   static Elf_Phdr ph;
 
   ramdisk_read(&eh, 0, 52);
 
   //
-  printf("eh: %d\n", eh.e_phoff);
-  printf("eh: %d\n", eh.e_phentsize);
-  printf("eh: %d\n", eh.e_phnum);
+  // printf("eh: %d\n", eh.e_phoff);
+  // printf("eh: %d\n", eh.e_phentsize);
+  // printf("eh: %d\n", eh.e_phnum);
 
   for (int i = 0; i < eh.e_phnum; i ++) {
     ramdisk_read(&ph, eh.e_phoff + i * eh.e_phentsize, eh.e_phentsize);
 
     //
-    printf("ph: %d\n", ph.p_type);
-    printf("ph: %p\n", ph.p_offset);
-    printf("ph: %p\n", ph.p_vaddr);
-    printf("ph: %d\n", ph.p_filesz);
-    printf("ph: %d\n", ph.p_memsz);
-    printf("ph: \n");
+    // printf("ph: %d\n", ph.p_type);
+    // printf("ph: %p\n", ph.p_offset);
+    // printf("ph: %p\n", ph.p_vaddr);
+    // printf("ph: %d\n", ph.p_filesz);
+    // printf("ph: %d\n", ph.p_memsz);
+    // printf("ph: \n");
     
     if (ph.p_type == 1) {
       ramdisk_read((void(*))ph.p_vaddr, ph.p_offset, ph.p_filesz);
