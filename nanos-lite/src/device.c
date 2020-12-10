d7c46db877fef3131f54d80b12a433c755b2eba6
tracer-ics2020 <tracer@njuics.org>
2020-12-10 22:06:12 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 538486c8e30e4fec8155dbb1bbf5cd15   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  22:06:12 up  8:16,  1 user,  load average: 0.54, 0.76, 0.83

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -38,7 +38,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 
   int x = offset / dispinfo.width, y = offset % dispinfo.width;
 
-  printf("fbw: %d %d\n", x, y);
+  printf("fbw: %d %d %d\n", x, y, len);
 	io_write(AM_GPU_FBDRAW, offset / dispinfo.width, offset % dispinfo.width, buf, len, 1, true);
   return 0;
 }
