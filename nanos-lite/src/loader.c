fdde6007d555631d1483b5f5b3fee072647cfb13
tracer-ics2020 <tracer@njuics.org>
2020-12-10 20:12:24 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 538486c8e30e4fec8155dbb1bbf5cd15   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  20:12:24 up  6:22,  1 user,  load average: 1.48, 1.03, 0.86

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,7 +15,7 @@ size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  printf("loading\n");
+  printf("loading %s\n", filename);
   int fd = fs_open(filename, 0, 0);
   static Elf_Ehdr eh;
   static Elf_Phdr ph;
