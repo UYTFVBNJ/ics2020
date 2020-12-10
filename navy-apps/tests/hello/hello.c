9e10539303bcfa1d8d9921b0d764687b63455fc5
tracer-ics2020 <tracer@njuics.org>
2020-12-10 15:02:23 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 538486c8e30e4fec8155dbb1bbf5cd15   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  15:02:23 up  1:12,  1 user,  load average: 0.76, 0.57, 0.51

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <NDL.h>
 
 int main() {
   if (write(1, "Hello World!\n", 13) == 0) puts("OK");
@@ -11,12 +11,16 @@ int main() {
   int i = 2;
   volatile int j = 0;
   while (1) {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    if (tv.tv_usec < 1500) {
+    // struct timeval tv;
+    // gettimeofday(&tv, NULL);
+
+    uint32_t time = NDL_GetTicks();
+    // if (tv.tv_usec < 1500) {
+    if (time <= 2) {
 
       printf("Hello World from Navy-apps at %dth time!\n", i ++);
-      printf("%d %d\n", tv.tv_sec, tv.tv_usec);
+      // printf("%d %d\n", tv.tv_sec, tv.tv_usec);
+      printf("%d\n", time);
 
 
       j ++;
