781cd0111aca68c884fcb8ff965314fcb9167b0d
tracer-ics2020 <tracer@njuics.org>
2020-12-10 20:11:41 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 538486c8e30e4fec8155dbb1bbf5cd15   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  20:11:41 up  6:21,  1 user,  load average: 0.84, 0.85, 0.79

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,7 +29,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_GPU_CONFIG_T di = io_read(AM_GPU_CONFIG);
+  return snprintf(buf, len + 1,"WIDTH : %d \nHEIGHT : %d\n", di.width, di.height);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -11,7 +11,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, DEV_EVT, CTL_FB, DEV_FB, PROC_DIF,};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -25,14 +25,17 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
-  [FD_FB]     = {"/dev/fb", 0, 0, invalid_read, invalid_write},
-  [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
+  [DEV_EVT]   = {"/dev/events", 0, 0, events_read, invalid_write},
+  [CTL_FB]    = {"???fbctl", 0, 0, invalid_read, invalid_write},
+  [DEV_FB]    = {"/dev/fb", 0, 0, invalid_read, invalid_write},
+  [PROC_DIF]  = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
@@ -41,7 +44,7 @@ static Finfo file_table[] __attribute__((used)) = {
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
-size_t open_offset[sizeof(file_table)];
+size_t open_offset[FT_SIZE];
 
 void init_fs() {
   Log("Initializing file system");
