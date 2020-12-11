00f4859df1958bdb3f12b1571a3467a423b3921d
tracer-ics2020 <tracer@njuics.org>
2020-12-11 23:14:03 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  23:14:03 up  2:13,  1 user,  load average: 1.46, 1.31, 1.27

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -38,6 +38,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
     int nread = fread(&pixels[w * i], 3, w, fp);
+  assert(pixels);
     for (int j = w - 1; j >= 0; j --) {
       uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
       uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
@@ -45,8 +46,10 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
+  assert(pixels);
 
   fclose(fp);
+  assert(pixels);
   if (width) *width = w;
   if (height) *height = h;
   return pixels;
