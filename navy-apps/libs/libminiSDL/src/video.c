3b93c38f5dd99b343d051c60503e7767b42edbdf
tracer-ics2020 <tracer@njuics.org>
2020-12-12 12:31:52 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  12:31:52 up 15:30,  1 user,  load average: 1.19, 0.85, 0.67

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -7,9 +7,11 @@
 #include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
-  // printf("SDL_BlitSurface\n");
+  printf("SDL_BlitSurface\n");
   // printf("%p %p\n", srcrect, dstrect);
   printf("%d\n", src->format->BitsPerPixel);
+  printf("Surface: %d %d %d %d\n", src->format->Rshift, src->format->Gshift, src->format->Bshift, src->format->Ashift);
+  printf("Surface: %d %d %d %d\n", src->format->Rmask, src->format->Gmask, src->format->Bmask, src->format->Amask);
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
 
