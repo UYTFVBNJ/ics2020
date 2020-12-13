ff177dbc1a21c6bd77e1bf0725bac878569450bb
tracer-ics2020 <tracer@njuics.org>
2020-12-13 13:58:56 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  13:58:57 up 1 day, 16:58,  1 user,  load average: 0.91, 1.19, 1.18

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -85,13 +85,14 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     NDL_DrawRect(pixels_32, x, y, w, h);
   } else 
   if (s->format->BitsPerPixel == 8) {
+    printf("%d\n", sizeof(s->pixels));
     pixels_32 = malloc(sizeof(s->pixels) * 4);
 
     int k;
     for (int i = 0; i < h; i ++)
       for (int j = 0; j < w; j ++) 
-        // pixels_32[i * w + j] 
-        k
+        pixels_32[i * w + j] 
+        // k
           = s->format->palette->colors[((uint8_t *)s->pixels)[i * w + j]].val;
     
     NDL_DrawRect(pixels_32, x, y, w, h);
