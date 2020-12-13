eefb09948ba5468bba0c8661e0fa31800c03d98f
tracer-ics2020 <tracer@njuics.org>
2020-12-13 19:00:34 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  19:00:34 up 1 day, 21:59,  1 user,  load average: 1.50, 1.28, 1.11

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -19,7 +19,7 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  // printf("SDL_PollEvent\n");
+  printf("SDL_PollEvent\n");
   char buf[64];
   NDL_PollEvent(buf, 64);
   int i;
