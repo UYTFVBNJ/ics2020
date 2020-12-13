995f44cc7ec9b87069315e6bc122294a168dbec6
tracer-ics2020 <tracer@njuics.org>
2020-12-13 18:59:38 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:59:39 up 1 day, 21:58,  1 user,  load average: 1.37, 1.23, 1.09

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -31,7 +31,7 @@ int SDL_PollEvent(SDL_Event *ev) {
       ev->key.keysym.sym = i;
     }
     keystate[i] = 1;
-    printf("Poll : %d", i);
+    // printf("Poll : %d", i);
     return 1;
   } else 
   if (sscanf(buf, "ku %s", key) == 1) {
@@ -50,7 +50,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  // printf("SDL_WaitEvent\n");
+  printf("SDL_WaitEvent\n");
   while (SDL_PollEvent(event) == 0) {
   }
 
