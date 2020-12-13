7672bbfed6c5d896c48dce6be0d8591314bfaea9
tracer-ics2020 <tracer@njuics.org>
2020-12-13 18:41:25 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:41:25 up 1 day, 21:40,  1 user,  load average: 1.23, 0.95, 0.85

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -72,16 +72,15 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
-  if (fd > 6 && open_offset[fd] + len >= file_table[fd].size) 
+  if (fd > 6 && open_offset[fd] + len >= file_table[fd].size) // fd > 6 might be changed
     len = file_table[fd].size - open_offset[fd];
-  if (fd == 6) printf("::%d\n", len);
   size_t l = file_table[fd].read(buf, file_table[fd].disk_offset + open_offset[fd], len);
   open_offset[fd] += l;
   return l;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  if (fd > 6 && open_offset[fd] + len >= file_table[fd].size) 
+  if (fd > 6 && open_offset[fd] + len >= file_table[fd].size) // fd > 6 might be changed
     len = file_table[fd].size - open_offset[fd];
 
   size_t l = file_table[fd].write(buf, file_table[fd].disk_offset + open_offset[fd], len);
