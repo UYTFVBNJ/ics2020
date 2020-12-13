ce65a6cbb46d0c55e9b4cdd74990b499d800a667
tracer-ics2020 <tracer@njuics.org>
2020-12-13 10:31:00 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  10:31:00 up 1 day, 13:30,  1 user,  load average: 0.61, 0.87, 0.84

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
-  printf("SDL_BlitSurface\n");
+  // printf("SDL_BlitSurface\n");
   // printf("%p %p\n", srcrect, dstrect);
   // printf("%d\n", src->format->BitsPerPixel);
   // printf("Surface: %d %d %d %d\n", src->format->Rshift, src->format->Gshift, src->format->Bshift, src->format->Ashift);
@@ -23,17 +23,17 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
   uint32_t *dst_pixels = (uint32_t *)dst->pixels, *src_pixels = (uint32_t *)src->pixels; 
 
-  printf("Surface: %d %d %d %d\n", dst_sf_w, dst_sf_h, src_sf_w, src_sf_h);
-  printf("Surface: %d %d %d %d\n", dst_x, dst_y, src_h, src_w);
+  // printf("Surface: %d %d %d %d\n", dst_sf_w, dst_sf_h, src_sf_w, src_sf_h);
+  // printf("Surface: %d %d %d %d\n", dst_x, dst_y, src_h, src_w);
   for (int i = 0; i < src_h; i ++) 
     for (int j = 0; j < src_w; j ++) {
       dst_pixels[(dst_y + i) * dst_sf_w + dst_x + j] = src_pixels[(src_y + i) * src_sf_w + src_x + j];
-      printf("pixels: %d %d %d %d\n", (dst_y + i), dst_x + j, (dst_y + i) * dst_sf_w + dst_x + j, (src_y + i) * src_sf_w + src_x + j);
+      // printf("pixels: %d %d %d %d\n", (dst_y + i), dst_x + j, (dst_y + i) * dst_sf_w + dst_x + j, (src_y + i) * src_sf_w + src_x + j);
     }
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-  printf("SDL_FillRect\n");
+  // printf("SDL_FillRect\n");
   // printf("%x\n", color);
   // printf("%p\n", dstrect);
 
@@ -47,7 +47,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  printf("UpdateRect: %d %d %d %d\n", x, y, w, h);
+  // printf("UpdateRect: %d %d %d %d\n", x, y, w, h);
   // printf("Surface: %d\n", s->format->BitsPerPixel);
   // printf("Surface: %d %d %d\n", s->format->Rshift, s->format->Gshift, s->format->Bshift);
   // printf("Surface: %d %d %d\n", s->format->Rmask, s->format->Gmask, s->format->Bmask);
