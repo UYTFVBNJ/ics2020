07b070390ea534671089e4e8a93a2e18d9ef4d58
tracer-ics2020 <tracer@njuics.org>
2020-12-13 10:17:00 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  10:17:00 up 1 day, 13:16,  1 user,  load average: 1.02, 0.70, 0.60

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -23,8 +23,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
   uint32_t *dst_pixels = (uint32_t *)dst->pixels, *src_pixels = (uint32_t *)src->pixels; 
 
-  // printf("Surface: %d %d\n", dst_sf_w, dst_sf_h);
-  // printf("Surface: %d %d %d %d\n", dst_x, dst_y, src_h, src_w);
+  printf("Surface: %d %d\n", dst_sf_w, dst_sf_h);
+  printf("Surface: %d %d %d %d\n", dst_x, dst_y, src_h, src_w);
   for (int i = 0; i < src_h; i ++) 
     for (int j = 0; j < src_w; j ++) 
       dst_pixels[(dst_y + i) * dst_sf_w + dst_x + j] = src_pixels[(src_y + i) * src_sf_w + src_x + j];
