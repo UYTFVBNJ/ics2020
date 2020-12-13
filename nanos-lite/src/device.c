5c211e1cff85f7a2dfd5e0ad6474a3243fa50b67
tracer-ics2020 <tracer@njuics.org>
2020-12-13 18:18:58 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:18:58 up 1 day, 21:18,  1 user,  load average: 1.14, 0.81, 0.88

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -30,6 +30,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 
 AM_GPU_CONFIG_T dispinfo; // might be changed
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  printf("%d\n", len);
   dispinfo = io_read(AM_GPU_CONFIG);
   printf("WIDTH : %d \nHEIGHT : %d\n", dispinfo.width, dispinfo.height);
   printf("%d\n", len);
