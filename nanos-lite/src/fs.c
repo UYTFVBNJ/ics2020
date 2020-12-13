9a9f07f72cc14f9ea5053ff60b9397e104693698
tracer-ics2020 <tracer@njuics.org>
2020-12-13 18:53:24 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 164f7d5ed53f41b7a792a7fe823800d3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:53:24 up 1 day, 21:52,  1 user,  load average: 1.03, 1.17, 1.01

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -89,6 +89,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   size_t l = file_table[fd].write(buf, file_table[fd].disk_offset + open_offset[fd], len);
   
   open_offset[fd] += l;
+  printf("fs_write: %d\n", fd);
   return l;
 }
 
