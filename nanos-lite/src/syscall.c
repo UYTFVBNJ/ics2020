fb0d0d854405003efcf89cc4929e8ca0713d2c48
tracer-ics2020 <tracer@njuics.org>
2021-01-19 20:57:22 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 1d417164654b44de8fd053af515d8273   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  20:57:22 up  1:02,  1 user,  load average: 0.97, 0.65, 0.62

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,7 @@
 #include "syscall.h"
 
 void naive_uload(PCB *, const char *);
-inline void SYS_execve_handler(Context *c) {
+inline Context* SYS_execve_handler(Context *c) {
 
   const char * pathname = (char *)c->GPR2;
   // char * const argv = (char *)c->GPR3;
@@ -14,22 +14,25 @@ inline void SYS_execve_handler(Context *c) {
   c->GPRx = 0;
 
   c->epc += 4;
+  return c;
 }
 
-inline void SYS_exit_handler(Context *c) {
+inline Context* SYS_exit_handler(Context *c) {
   c->GPR2 = (uintptr_t)&"/bin/nterm";
   SYS_execve_handler(c);
+  return c;
 }
 
-inline void SYS_yield_handler(Context *c) {
+inline Context* SYS_yield_handler(Context *c) {
   yield();
 
   c->epc += 4;
+  return c;
 }
 
 int fs_open(const char *pathname, int flags, int mode);
 
-inline void SYS_open_handler(Context *c) {
+inline Context* SYS_open_handler(Context *c) {
   const char * path = (char*)c->GPR2;
   int flags = c->GPR3;
   int mode = c->GPR4;
@@ -37,12 +40,13 @@ inline void SYS_open_handler(Context *c) {
   c->GPRx = fs_open(path, flags, mode);
 
   c->epc += 4;
+  return c;
 }
 
 
 size_t fs_read(int fd, void *buf, size_t len);
 
-inline void SYS_read_handler(Context *c) {
+inline Context* SYS_read_handler(Context *c) {
   int fd = c->GPR2;
   void * buf = (void*)c->GPR3;
   size_t len = c->GPR4;
@@ -50,11 +54,12 @@ inline void SYS_read_handler(Context *c) {
   c->GPRx = fs_read(fd, buf, len);
 
   c->epc += 4;
+  return c;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len);
 
-inline void SYS_write_handler(Context *c) {
+inline Context* SYS_write_handler(Context *c) {
   int fd = c->GPR2;
   const char * buf = (char*)c->GPR3;
   size_t count = c->GPR4;
@@ -68,21 +73,23 @@ inline void SYS_write_handler(Context *c) {
   }
 
   c->epc += 4;
+  return c;
 }
 
 int fs_close(int fd);
 
-inline void SYS_close_handler(Context *c) {
+inline Context* SYS_close_handler(Context *c) {
   int fd = c->GPR2;
 
   c->GPRx = fs_close(fd);
 
   c->epc += 4;
+  return c;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence);
 
-inline void SYS_lseek_handler(Context *c) {
+inline Context* SYS_lseek_handler(Context *c) {
   int fd = c->GPR2;
   size_t offset =  c->GPR3;
   int whence = c->GPR4;
@@ -90,6 +97,7 @@ inline void SYS_lseek_handler(Context *c) {
   c->GPRx = fs_lseek(fd, offset, whence);
 
   c->epc += 4;
+  return c;
 }
 
 inline Context* SYS_brk_handler(Context *c) {
@@ -106,7 +114,7 @@ struct timeval {
   __time_t tv_sec;		/* Seconds.  */
   __suseconds_t tv_usec;	/* Microseconds.  */
 } * tv;
-inline void SYS_gettimeofday_handler(Context *c) {
+inline Context* SYS_gettimeofday_handler(Context *c) {
 
   AM_TIMER_UPTIME_T rtc = io_read(AM_TIMER_UPTIME);
 
@@ -118,6 +126,7 @@ inline void SYS_gettimeofday_handler(Context *c) {
   c->GPRx = 0;
 
   c->epc += 4;
+  return c;
 }
 
 #define SYS_handle(x) case SYS_ ## x: return SYS_ ## x ## _handler(c);     break
@@ -127,19 +136,19 @@ Context* do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    // SYS_handle(exit);
-    // SYS_handle(yield);
-    // SYS_handle(open);
-    // SYS_handle(read);
-    // SYS_handle(write);
-    // SYS_handle(close);
-    // SYS_handle(lseek);
+    SYS_handle(exit);
+    SYS_handle(yield);
+    SYS_handle(open);
+    SYS_handle(read);
+    SYS_handle(write);
+    SYS_handle(close);
+    SYS_handle(lseek);
     SYS_handle(brk);
 
-    // SYS_handle(execve);
+    SYS_handle(execve);
   
 
-    // SYS_handle(gettimeofday);
+    SYS_handle(gettimeofday);
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
