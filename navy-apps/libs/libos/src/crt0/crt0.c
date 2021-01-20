535e5a56d90a8ba7f22e456fccd7be4f06f27b24
tracer-ics2020 <tracer@njuics.org>
2021-01-20 21:33:52 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 9ac0a2587c07452faa0061fcd5961fbd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  21:33:52 up  4:38,  1 user,  load average: 1.25, 1.08, 0.92

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -4,15 +4,15 @@
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
-/*
+
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
   exit(main(0, empty, empty));
   assert(0);
 }
-*/
 
+/*
 void call_main(uintptr_t *args) {
   printf("call_main\n");
   int argc = (int)*args;
@@ -22,3 +22,4 @@ void call_main(uintptr_t *args) {
   exit(main(argc, argv, envp));
   assert(0);
 }
+*/
