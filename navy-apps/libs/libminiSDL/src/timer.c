455617dcb8a2f63f1371f4e338a0401d9f9204ad
tracer-ics2020 <tracer@njuics.org>
2021-01-20 21:26:48 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 9ac0a2587c07452faa0061fcd5961fbd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  21:26:48 up  4:31,  1 user,  load average: 0.82, 0.87, 0.78

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -3,22 +3,22 @@
 #include <stdio.h>
 
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
-  printf("SDL_AddTimer\n");
+  // printf("SDL_AddTimer\n");
   return NULL;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
-  printf("SDL_RemoveTimer\n");
+  // printf("SDL_RemoveTimer\n");
   return 1;
 }
 
 uint32_t SDL_GetTicks() {
-  printf("SDL_GetTicks\n");
+  // printf("SDL_GetTicks\n");
   return NDL_GetTicks();
 }
 
 void SDL_Delay(uint32_t ms) {
-  printf("SDL_Delay\n");
+  // printf("SDL_Delay\n");
   uint32_t start_time = SDL_GetTicks();
 
   while (SDL_GetTicks() < start_time + ms) {}
