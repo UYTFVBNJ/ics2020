a0f411e21086c239bb248da11ee9b353fe9a5f26
tracer-ics2020 <tracer@njuics.org>
2021-01-20 17:27:32 +0800

    >  compile 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 9ac0a2587c07452faa0061fcd5961fbd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  17:27:32 up 32 min,  1 user,  load average: 0.40, 0.47, 0.47

--- abstract-machine/am/src/nemu/isa/riscv32/cte.c
+++ abstract-machine/am/src/nemu/isa/riscv32/cte.c
@@ -7,6 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   // printf("__am_irq_handle:");
   printf("%p %d %d\n", c->epc, c->status, c->cause);
+  printf("%p %p\n", c, c->gpr[2]);
   // for (int i = 0; i < 32; i ++) printf("%d ", c->gpr[i]); 
   // printf("\n");
   // for (int i = 0; i < 32; i ++) printf("%d ", &c->gpr[i]); 
@@ -38,6 +39,7 @@ Context* __am_irq_handle(Context *c) {
   }
 
   printf("%p %d %d\n", c->epc, c->status, c->cause);
+  printf("%p %p\n", c, c->gpr[2]);
   
 
   return c;
