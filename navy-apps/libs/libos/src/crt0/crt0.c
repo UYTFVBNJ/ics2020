f18882afe5b01a98aea17fbe42dad25c35c379cc
tracer-ics2020 <tracer@njuics.org>
2021-01-20 22:07:37 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 9ac0a2587c07452faa0061fcd5961fbd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  22:07:37 up  5:12,  1 user,  load average: 1.14, 1.01, 0.91

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,26 +6,10 @@
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
-/*
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
-  assert(0);
-}
-*/
-
-void call_main(uintptr_t *args) {
-  printf("call_main\n");
   int argc = (int)*args;
   char ** argv = (char **)(args + 1);
   char ** envp = (char **)(args + 1 + argc + 1); 
-  printf("envp[0] %p\n", envp[0]);
-  printf("argv[0] %p\n", argv[0]);
-  printf("argv[1] %p\n", argv[1]);
-  printf("argv[2] %p\n", argv[2]);
-  // char *empty[] =  {NULL };
-  // environ = empty;
   environ = envp;
   exit(main(argc, argv, envp));
   assert(0);
