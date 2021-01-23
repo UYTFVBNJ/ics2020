f18aa8a9adf93778771db33f8cee889462c6f33f
tracer-ics2020 <tracer@njuics.org>
2021-01-23 11:02:09 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 6c3712f0145448669e7448d0b3c0a917   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  11:02:09 up 7 min,  1 user,  load average: 0.76, 0.70, 0.45

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -88,7 +88,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   
   int ustk_envp_len = 0;
   char * ustk_envp[10];
-  // if (envp != NULL)
+  if (envp != NULL)
   for (ustk_envp_len = 0; envp[ustk_envp_len] != NULL; ustk_envp_len ++) {
     printf("uload envp[%d] : %s\n", ustk_envp_len, envp[ustk_envp_len]);
     size_t len;
@@ -103,7 +103,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
   int ustk_argv_len = 0;
   char * ustk_argv[10];
-  // if (argv != NULL)
+  if (argv != NULL)
   for (ustk_argv_len = 0; argv[ustk_argv_len] != NULL; ustk_argv_len ++) {
     printf("uload argv[%d] : %s\n", ustk_argv_len, argv[ustk_argv_len]);
     size_t len;
