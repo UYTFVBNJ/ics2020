28eac47b900d32e288a8a16cf9acd65c62fd43d4
tracer-ics2020 <tracer@njuics.org>
2021-01-23 14:08:20 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: a5d2aecad0254a6fb2a3f33111ad71b6   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  14:08:20 up 46 min,  1 user,  load average: 0.86, 0.69, 0.65

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,7 +12,7 @@ inline Context* SYS_execve_handler(Context *c) {
   char ** const argv = (char **)c->GPR3;
   char ** const envp = (char **)c->GPR4;
 
-  printf("SYS: envp[0] %p : %p\n", envp, envp[0]);
+  printf("SYS: envp[0] %p : %p %s\n", envp, envp[0], envp[0]);
   // naive_uload(NULL, pathname);
   context_uload(current, pathname, argv, envp);
   switch_boot_pcb();
