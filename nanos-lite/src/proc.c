be0b0ea2b1607645a282c5f917bf70a53d438cb3
tracer-ics2020 <tracer@njuics.org>
2021-01-24 12:47:14 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 25a446734543419e945a000b55a671ae   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  12:47:15 up 21:18,  1 user,  load average: 0.77, 0.52, 0.43

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,13 +26,13 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
 void init_proc() {
   // context_kload(&pcb[0], hello_fun, NULL);
-  context_kload(&pcb[0], hello_fun, (void*)0x1000);
+  // context_kload(&pcb[0], hello_fun, (void*)0x1000);
   context_kload(&pcb[1], hello_fun, (void*)0x1111);
-  // char * argv[] = {"--skip", NULL};
+  char * argv[] = {"--skip", NULL};
   // char * argv[] = {NULL};
   // char * argv[] = {"/bin/exec-test", NULL};
-  // char * envp[] = {NULL};
-  // context_uload(&pcb[0], "/bin/pal", argv, envp);
+  char * envp[] = {NULL};
+  context_uload(&pcb[0], "/bin/pal", argv, envp);
   // context_uload(&pcb[0], "/bin/dummy", argv, envp);
   // context_uload(&pcb[0], "/bin/exec-test", argv, envp);
   // context_uload(&pcb[0], "/bin/nterm", argv, envp);
