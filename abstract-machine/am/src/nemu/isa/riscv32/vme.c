e5d841cd8c98d861fce5ba5150e1b17814d7357b
tracer-ics2020 <tracer@njuics.org>
2021-01-24 12:38:21 +0800

    >  run 191250034    Static hostname: ENIAC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8559be8b393b4ef2b5df3d51fd6f52bc            Boot ID: 25a446734543419e945a000b55a671ae   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-38-generic       Architecture: x86-64  12:38:21 up 21:09,  1 user,  load average: 0.86, 0.49, 0.37

--- abstract-machine/am/src/nemu/isa/riscv32/vme.c
+++ abstract-machine/am/src/nemu/isa/riscv32/vme.c
@@ -100,6 +100,7 @@ void unprotect(AddrSpace *as) {
 }
 
 void __am_get_cur_as(Context *c) {
+    printf("am_get: %p\n", c->pdir);
   if (vme_enable && c->pdir != NULL) {
     printf("am_get: %p\n", c->pdir);
     c->pdir = (vme_enable ? (void *)get_satp() : NULL);
